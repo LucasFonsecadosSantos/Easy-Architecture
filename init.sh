a4423390b8e36693957ae498f44d35c69685a25f
Lucas Fonseca dos Santos <lucas@lcfcompany.com.br>
2018-03-05 03:29:31 -0500

    It works FINAL

--- init.sh
+++ init.sh
@@ -13,11 +13,12 @@ echo 'Script written by Lucas Fonseca dos Santos.'
 echo 'lucas@lcfcompany.com.br'
 echo ''
 sleep 2
+
 if ! [ -x "$(command -v sass)" ]; then
 	echo '[X] ERROR: The SASS pre compiler package is not installed!' >&2
 	exit 1
 else
-	echo '[OK] SASS is installed.'
+	echo '[ OK] SASS is installed.'
 fi
 
 if ! [ -x "$(command -v pug)" ]; then
@@ -31,14 +32,17 @@ if ! [ -x "$(command -v ruby)" ]; then
 else
 	echo '[OK] Ruby is installed.'
 fi
+
 echo ''
 echo '[..] Creating the directories'
 mkdir -p $HOME/$1/src/
+
 if [ -d $HOME/$1 ]; then
 	echo '[OK] The project main directory was successfully created in: ' $HOME/$1
 else
 	echo '[X] ERROR: There was some problem with the directories creation.'
 fi
+
 echo ''
 echo '[..] Creating the remaining directories'
 mkdir -p $HOME/$1/src/ext/sass/libs
@@ -61,11 +65,13 @@ echo '[..] Downloading the Bootstrap version 4'
 sudo wget --directory-prefix=$HOME/$1/src/ext/sass/ http://github.com/twbs/bootstrap/archive/v4.0.0-alpha.6.zip
 echo '[OK] Download successfully!'
 echo '[..] Extracting the Bootstrap core files'
+
 for entry in "$HOME/$1/src/ext/sass"/*.zip
 do
 	sudo unzip -d $HOME/$1/src/ext/sass $entry
 	echo '[OK] Files extracted.'
 done
+
 echo '[..] Copying the Bootstrap core files to libs directory.'
 sudo mv $HOME/$1/src/ext/sass/bootstrap-4.0.0-alpha.6/scss/* $HOME/$1/src/ext/sass/libs/
 echo '[OK] Files copied.'
@@ -73,4 +79,6 @@ echo '[..] Deleting the zip source files.'
 sudo rm -r $HOME/$1/src/ext/sass/bootstrap-4.0.0-alpha.6
 sudo rm $HOME/$1/src/ext/sass/v4.0.0-alpha.6.zip
 echo '[OK] Zip file deleted.'
-
+echo '[..] Copying the SASS and PUG compiler bash scripts to ' $HOME/$s1 ' directory.'
+sudo cp -avr ./compile_sass.sh ./compile_pug.sh $HOME/$1/
+echo '[OK] Compiler files copied!'
